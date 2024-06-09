aefd7a5c49a9d086ff9740268af2e717853cd3cb
Hieronimus Pradnya Narendra <hieropradnya77@gmail.com>
2024-06-09 17:47:38 +0700

    dashboard cukup selesai

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kendaraan;
+use App\Models\Pembayaran;
+use App\Models\Pesanan;
+use App\Models\User;
+use App\Models\Voucher;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    function index(){
-        return view('dashboard/index');
+    // function index(){
+    //     return view('dashboard/index');
+    // }
+
+    public function index()
+    {
+        $currentDateTime = now();
+        // Get ongoing orders with status 'diterima'
+        $ongoingOrders = Pesanan::where('status_pesanan', 'diterima')
+            ->where('waktu_mulai', '<=', $currentDateTime)
+            ->where('waktu_selesai', '>=', $currentDateTime)
+            ->with('kendaraan', 'users')
+            ->get();
+
+
+
+        $totalUsers = User::count();
+        $totalKendaraan = Kendaraan::count();
+        $totalPesanan = Pesanan::count();
+        $totalVoucher = Voucher::count();
+        $totalPembayaran = Pembayaran::count();
+
+        $recentPesanan = Pesanan::with('users', 'kendaraan')
+            ->orderBy('created_at', 'desc')
+            ->limit(5)
+            ->get();
+
+        return view('dashboard/index', compact('totalUsers', 'totalKendaraan', 'totalPesanan', 'totalVoucher', 'totalPembayaran', 'recentPesanan', 'ongoingOrders'));
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,88 +1,183 @@
-@extends('layout/admin')
+@extends('layout.admin')
 
 @section('konten')
-    <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Selamat datang {{ Auth::user()->nama }}</h1>
-        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
-    </div>
+    <div class="container-fluid">
 
-    
-    <div class="row">
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Selamat datang {{ Auth::user()->nama }}.</h1>
+        </div>
 
-        <!-- Area Chart -->
-        <div class="col-xl-8 col-lg-7">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div
-                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
-                    <div class="dropdown no-arrow">
-                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                            aria-labelledby="dropdownMenuLink">
-                            <div class="dropdown-header">Dropdown Header:</div>
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
+        <!-- Content Row -->
+        <div class="row">
+
+            <!-- Total Users Card -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                    Total Users</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalUsers }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-users fa-2x text-gray-300"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                    <div class="chart-area">
-                        <canvas id="myAreaChart"></canvas>
-                    </div>
-                </div>
             </div>
-        </div>
 
-        <!-- Pie Chart -->
-        <div class="col-xl-4 col-lg-5">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div
-                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
-                    <div class="dropdown no-arrow">
-                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                            aria-labelledby="dropdownMenuLink">
-                            <div class="dropdown-header">Dropdown Header:</div>
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
+            <!-- Total Kendaraan Card -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                    Total Kendaraan</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalKendaraan }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-car fa-2x text-gray-300"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                    <div class="chart-pie pt-4 pb-2">
-                        <canvas id="myPieChart"></canvas>
-                    </div>
-                    <div class="mt-4 text-center small">
-                        <span class="mr-2">
-                            <i class="fas fa-circle text-primary"></i> Direct
-                        </span>
-                        <span class="mr-2">
-                            <i class="fas fa-circle text-success"></i> Social
-                        </span>
-                        <span class="mr-2">
-                            <i class="fas fa-circle text-info"></i> Referral
-                        </span>
+            </div>
+
+            <!-- Total Pesanan Card -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-info shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Total Pesanan
+                                </div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalPesanan }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-shopping-cart fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+
+            <!-- Total Voucher Card -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-warning shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                    Total Voucher</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalVoucher }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-ticket-alt fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
+
+
+
+        <!-- Row for Orders -->
+
+        <div class="row">
+            <!-- Card for each order -->
+            @foreach ($ongoingOrders as $order)
+                <div class="col-xl-4 col-md-6 mb-4">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        Pesanan ID: {{ $order->pesanan_id }}</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800 text-uppercase">
+                                        {{ $order->kendaraan->merk }}
+                                        {{ $order->kendaraan->tipe }}
+                                        {{ $order->kendaraan->tahun_produksi }}</div>
+                                    <div class="text-xs text-gray-800">
+                                        <strong>Pelanggan:</strong> {{ $order->users->nama }}<br>
+
+                                        <strong>Waktu Mulai:</strong>
+                                        {{ \Carbon\Carbon::parse($order->waktu_mulai)->locale('id')->isoFormat('HH:mm | D MMMM Y') }}<br>
+                                        <strong>Waktu Selesai:</strong>
+                                        {{ \Carbon\Carbon::parse($order->waktu_selesai)->locale('id')->isoFormat('HH:mm | D MMMM Y') }}<br>
+                                        <strong>Harga Sewa:</strong>
+                                        Rp{{ number_format($order->kendaraan->harga_sewa, 0, ',', '.') }}<br>
+                                        <strong>Plat Nomor:</strong> {{ $order->kendaraan->plat_nomor }}
+                                    </div>
+                                </div>
+                                <div class="col-auto">
+                                    <img src="{{ asset('storage/' . $order->kendaraan->foto) }}" class="img-fluid rounded"
+                                        alt="{{ $order->kendaraan->merk }}" style="width: 100px;">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+
+
+
+
+
+        <!-- Content Row -->
+        <div class="row">
+
+            <!-- Recent Orders Table -->
+            <div class="col-lg-12">
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-primary">Pesanan Terbaru</h6>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                <thead>
+                                    <tr>
+                                        <th>ID Pesanan</th>
+                                        <th>Pelanggan</th>
+                                        <th>Kendaraan</th>
+                                        <th>Waktu Mulai</th>
+                                        <th>Waktu Selesai</th>
+                                        <th>Status Pesanan</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($recentPesanan as $pesanan)
+                                        <tr>
+                                            <td>P{{ $pesanan->pesanan_id }}</td>
+                                            <td class="text-capitalize">{{ $pesanan->users->nama }}</td>
+                                            <td class="text upper-case">{{ $pesanan->kendaraan->merk }}
+                                                {{ $pesanan->kendaraan->tipe }}</td>
+                                            <td>{{ $pesanan->waktu_mulai }}</td>
+                                            <td>{{ $pesanan->waktu_selesai }}</td>
+                                            <td>{{ $pesanan->status_pesanan }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
     </div>
- 
+    <script>
+        $(document).ready(function() {
+            $('#dataTable').DataTable();
+        });
+    </script>
 @endsection
